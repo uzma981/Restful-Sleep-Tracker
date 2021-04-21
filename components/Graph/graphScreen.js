6f3cf0e8f3e18f8b7eeb58b8c7c275534f2bd15d
Uzma89 <uzma.a786@hotmail.com>
2021-04-21 16:34:39 +0100

    added a graph to the graph screen

--- components/Graph/graphScreen.js
+++ components/Graph/graphScreen.js
@@ -1,15 +1,84 @@
 import * as React from 'react';
-import {View, Text , Image, StyleSheet, TouchableOpacity,Linking} from 'react-native';
+import {View, Text , Dimensions,Image, StyleSheet, TouchableOpacity,Linking} from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
-
-
+import {LinearGradient} from 'expo-linear-gradient';
+import {
+  LineChart,
+  BarChart,
+  PieChart,
+  ProgressChart,
+  ContributionGraph,
+  StackedBarChart
+} from "react-native-chart-kit";
 
 
 
 function GraphScreen({ navigation }) {
+  const chartConfig = {
+    backgroundGradientFrom: "#1E2923",
+    backgroundGradientFromOpacity: 0,
+    backgroundGradientTo: "#08130D",
+    backgroundGradientToOpacity: 0.5,
+    color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
+    strokeWidth: 2, // optional, default 3
+    barPercentage: 0.5,
+    useShadowColorFromDataset: false // optional
+  };
     return (
 
      <View style={styles.container}>
+     <LinearGradient colors={['#003049','#581a87']} style={{flex:1}}>
+
+<View>
+ 
+  <LineChart
+    data={{
+      labels: ["Mon", "Tues", "Wed", "Thurs", "Fri", "Sat","Sun"],
+      datasets: [
+        {
+          data: [
+            Math.random() * 100,
+            Math.random() * 100,
+            Math.random() * 100,
+            Math.random() * 100,
+            Math.random() * 100,
+            Math.random() * 100
+          ]
+        }
+      ]
+    }}
+    width={Dimensions.get("window").width} // from react-native
+    height={220}
+   // yAxisLabel="h"
+    yAxisSuffix="h"
+    yAxisInterval={1} // optional, defaults to 1
+    chartConfig={{
+      backgroundColor: "#581a87",
+      backgroundGradientFrom: "#581a87",
+      backgroundGradientTo: "#2D187E",
+// decimalPlaces: 2, // optional, defaults to 2dp
+      color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+      labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+      style: {
+        borderRadius:50
+      },
+      propsForDots: {
+        r: "6",
+        strokeWidth: "3",
+        stroke: "#2D187E"
+      }
+    }}
+    bezier
+    style={{
+      marginVertical: 10,
+      borderRadius: 8
+    }}
+  />
+</View>
+
+
+
+
 <Text style={styles.textHeader}> 5 Tips for a Better Night's Sleep{' >'} </Text>
 
 <ScrollView horizontal
@@ -56,7 +125,7 @@ function GraphScreen({ navigation }) {
 </View>
 </TouchableOpacity>
 </ScrollView>
-
+</LinearGradient>
   </View>
  
 
@@ -109,7 +178,7 @@ function GraphScreen({ navigation }) {
       
     },
     textHeader:{
-      color: "black",
+      color: "white",
       marginTop: 20,
       fontSize: 17,
       marginHorizontal: 10,
